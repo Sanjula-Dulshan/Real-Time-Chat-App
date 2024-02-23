5d695d9a03fe7cf100165769afc606217b5537ae
Sanjula-Dulshan <sdulshan10@gmail.com>
2024-02-23 16:42:12 +0530

    sign up api

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -1,5 +1,42 @@
-export const signup = (req, res) => {
-  res.send("signup route");
+import User from "../models/user.model.js";
+
+export const signup = async (req, res) => {
+  try {
+    const { fullName, username, password, confirmPassword, gender } = req.body;
+
+    if (password !== confirmPassword) {
+      return res.status(400).json({ error: "Password don't match" });
+    }
+
+    const user = await User.findOne({ username });
+
+    if (user) {
+      return res.status(400).json({ error: "username already exists" });
+    }
+
+    const maleProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
+    const femaleProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
+
+    const newUser = new User({
+      fullName,
+      username,
+      password,
+      gender,
+      profilePicture: gender === "male" ? maleProfilePic : femaleProfilePic,
+    });
+
+    await newUser.save();
+
+    res.status(201).json({
+      _id: newUser._id,
+      fullName: newUser.fullName,
+      username: newUser.username,
+      profilePicture: newUser.profilePicture,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json({ error: error.message });
+  }
 };
 export const login = (req, res) => {
   res.send("Login route");
--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,18 @@
 import express from "express";
 import dotenv from "dotenv";
-import authRoutes from "./routes/auth.routes.js";
-const app = express();
 
-dotenv.config();
+import authRoutes from "./routes/auth.routes.js";
+import connectToMongoDB from "./db/connectToMongoDB.js";
+
+const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
+dotenv.config();
 
+app.use(express.json()); // to parse incoming requests with JSON payloads (from req.body)
 app.use("/api/auth", authRoutes);
 
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+app.listen(PORT, () => {
+  connectToMongoDB();
+  console.log(`Server is running on port ${PORT}`);
+});
